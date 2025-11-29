531b08b791c543010d8eaf14183b8fe7b026d917
Aporia2001 <josemilanes2001@gmail.com>
2025-11-28 19:17:44 -0500

    Add files via upload

--- script/cards-CustomAporia/c337.lua
+++ script/cards-CustomAporia/c337.lua
@@ -4,6 +4,14 @@ function s.initial_effect(c)
 	--Link summon
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,s.matfilter,4,nil,s.matcheck)
+	--Must first be Link Summoned
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetValue(aux.lnklimit)
+	c:RegisterEffect(e0)
 	--Unnafected by card effects
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
